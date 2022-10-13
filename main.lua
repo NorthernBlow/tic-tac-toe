6238f2874fa73e0f374e1ec49534e87e603e5289
NorthernBlow <rheinhart@yandex.ru>
2022-10-13 17:10:56 +0300

    first steps

---
+++ main.lua
@@ -0,0 +1,24 @@
+--[[
+Крестики-нолики
+
+Автор: Святослав Рэинхарт
+evolution.is.not.theory@gmail.com
+
+]]
+
+
+function love.load()
+  
+end
+
+
+
+function love.update(dt)
+  
+  
+end
+
+
+function love.draw()
+  
+  end
