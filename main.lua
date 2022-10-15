1381167d798d0b5c91d1081d91ea12c4d8408f67
NorthernBlow <rheinhart@yandex.ru>
2022-10-15 18:06:33 +0300

    add new logic

--- main.lua
+++ main.lua
@@ -7,10 +7,38 @@ evolution.is.not.theory@gmail.com
 ]]
 
 
+local push = require "push"
+
+
+-- JUST BECAUSE I LUVE AETHETIC
+VIRTUAL_WIDTH = 240
+VIRTUAL_HEIGHT = 320
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+local xSprite = love.graphics.newImage('graphics/crest.png')
+local oSprite = love.graphics.newImage('graphics/zero.png')
+
 function love.load()
-  
+  -- добавил немного размытия для более красивого отображения пикселей
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+  push:setupScreen(VIRTUAL_HEIGHT, VIRTUAL_WIDTH, WINDOW_WIDTH, WINDOW_HEIGHT, {
+      fullscreen = false,
+      vsync = true,
+      resizeble = true
+      })
 end
 
+
+
+function love.keypressed(key)
+  
+  if key == 'escape' then
+    love.event.quit()
+    end
+
+end
 
 
 function love.update(dt)
@@ -20,5 +48,7 @@ end
 
 
 function love.draw()
-  
+  push:start()
+  love.graphics.print('New game')
+  push:finish()
   end
