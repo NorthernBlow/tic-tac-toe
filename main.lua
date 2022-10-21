dc71ba90ed773f14d14504d9e6620ec9abc7da99
NorthernBlow <rheinhart@yandex.ru>
2022-10-21 04:07:29 +0300

    patch 0.76

--- main.lua
+++ main.lua
@@ -19,8 +19,10 @@ VIRTUAL_HEIGHT = 320
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
-MAP_HEIGHT, MAP_WIDTH = 3, 3
-MAP_TALE_SIZE = 40
+--маленькие квадраты
+mAP_HEIGHT, mAP_WIDTH = 3, 3
+mAP_TALE_SIZE = 40
+mAP_WIDTH_SIZE = 40
 
 SPRITE_PADDING = 6
 
@@ -88,40 +90,40 @@ end
 
 function drawMap()
   -- создаем карту 3 х 3
-  local xMargin = VIRTUAL_WIDTH - (MAP_TALE_SIZE * 3) -- 240 - 120 = 120
-  local yMargin = VIRTUAL_HEIGHT - (MAP_TALE_SIZE * 3) -- 320 - 120 = 200
+  local xMargin = VIRTUAL_WIDTH - (mAP_TALE_SIZE * 3)
+  local yMargin = VIRTUAL_HEIGHT - (mAP_WIDTH_SIZE * 3) 
   
   
   -- устанавливаем стиль линий
   love.graphics.setLineStyle('smooth')
   
   -- устанавливаем цвет линий
-  love.graphics.setColor(0, 1, 2)
+  --love.graphics.setColor(0, 1, 2)
   
   --устанавливаем ширину линий в 2 пикселя
   love.graphics.setLineWidth(2)
   
   -- первая вертикальная линия
-  love.graphics.line(xMargin / 2 + MAP_TALE_SIZE, yMargin / 2,
-    xMargin / 2 + MAP_TALE_SIZE, VIRTUAL_HEIGHT - yMargin / 2) -- x1=100, y1=100, x2=140, y2=220
+  love.graphics.line(xMargin / 2 + mAP_WIDTH_SIZE, yMargin / 2,
+    xMargin / 2 + mAP_TALE_SIZE, VIRTUAL_HEIGHT - yMargin / 2) -- x1=100, y1=100, x2=140, y2=220
   
   -- вторая вертикальная линия
-  love.graphics.line(VIRTUAL_WIDTH - xMargin / 2 - MAP_TALE_SIZE, yMargin / 2,
-    VIRTUAL_WIDTH - xMargin / 2 - MAP_TALE_SIZE, VIRTUAL_HEIGHT - yMargin / 2) -- x1=140, y1=100, x2=140, y2=220 (по х - линии отступили вправо на 40 пикселей относительно первой линии
+  love.graphics.line(VIRTUAL_WIDTH - xMargin / 2 - mAP_TALE_SIZE, yMargin / 2,
+    VIRTUAL_WIDTH - xMargin / 2 - mAP_WIDTH_SIZE, VIRTUAL_HEIGHT - yMargin / 2) -- x1=140, y1=100, x2=140, y2=220 (по х - линии отступили вправо на 40 пикселей относительно первой линии
   
   -- первая горизонтальная линия
-  love.graphics.line(xMargin / 2, yMargin / 2 + MAP_TALE_SIZE,
-    VIRTUAL_WIDTH - xMargin / 2, yMargin / 2 + MAP_TALE_SIZE) -- х1=60,у1=140,х2=180,у2=140
+  love.graphics.line(xMargin / 2, yMargin / 2 + mAP_TALE_SIZE,
+    VIRTUAL_WIDTH - xMargin / 2, yMargin / 2 + mAP_TALE_SIZE) -- х1=60,у1=140,х2=180,у2=140
   
   -- вторая горизонтальная линия
   local secondHorizontalLine = {60, 180, 180, 180} -- хватит пересчитывать переменные.
   love.graphics.line(secondHorizontalLine)
   
   -- рисует спрайты
-  for  y = 1, MAP_HEIGHT do
-    for x = 1, MAP_WIDTH do
-      local xOffset = xMargin / 2 + MAP_TALE_SIZE * (x - 1)
-      local yOffset = yMargin / 2 + MAP_TALE_SIZE * (y - 1)
+  for  y = 1, mAP_HEIGHT do
+    for x = 1, mAP_WIDTH do
+      local xOffset = xMargin / 2 + mAP_TALE_SIZE * (x - 1)
+      local yOffset = yMargin / 2 + mAP_WIDTH_SIZE * (y - 1)
       if map[y][x] == "" then
         -- ничего не делаем
       elseif map[y][x] == "X" then
@@ -131,8 +133,8 @@ function drawMap()
         love.graphics.draw(oSprite, xOffset + SPRITE_PADDING, yOffset)
       end
       if x == SelectedX and y == SelectedY then
-      love.graphics.setColor(0, 1, 1, 0.5)
-      love.graphics.rectangle('fill', xOffset, yOffset, MAP_TALE_SIZE, MAP_HEIGHT)
+      love.graphics.setColor(2, 1, 0, 0.5)
+      love.graphics.rectangle('fill', xOffset, yOffset, mAP_TALE_SIZE, mAP_WIDTH_SIZE)
       love.graphics.setColor(1, 1, 1, 1)
       end
     end
@@ -157,6 +159,7 @@ function love.draw()
   love.graphics.print(tostring(love.graphics.getRendererInfo('name', 'device')), 100, 1)
   love.graphics.print(tostring(1.0 / love.timer.getDelta()), 200, 1)
   drawMap()
-  love.graphics.print('Player ' .. currentPlayer .. "'s turn")
+  love.graphics.setColor(0, 1, 2);
+  love.graphics.print("Player " .. currentPlayer .. "'s turn")
   push:finish()
 end
